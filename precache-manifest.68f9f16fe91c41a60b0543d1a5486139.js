f70e7f228331a859cae4d36aeb7a3a596a81a65c
KelvinMitaki <mitakikelvin1@gmail.com>
2020-04-04 14:35:06 +0300

    Updates

---
+++ precache-manifest.68f9f16fe91c41a60b0543d1a5486139.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1ad8943a04c4d47fba9c3613b83be0",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "82a45b044e458ee3cb89",
+    "url": "/todo/static/js/2.07b4f19e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo/static/js/2.07b4f19e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b160665972d7093a17",
+    "url": "/todo/static/js/main.927880a7.chunk.js"
+  },
+  {
+    "revision": "ca69ba33643ba26bc5b3",
+    "url": "/todo/static/js/runtime-main.bce77707.js"
+  }
+]);
